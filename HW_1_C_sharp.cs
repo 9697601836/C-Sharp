515b447ace268ad12912c3b63a99406b3342de5d
9697601836 <vanya_bykov_1996@mail.ru>
2022-09-20 21:07:16 +0300

    add HW_1_C_sharp

---
+++ HW_1_C_sharp.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstSharpProgram
+{
+    class Program
+{
+    static void Main(string[] args) {
+        int x = 10;
+        int y = 25;
+        int z = x + y;
+        Console.WriteLine("x + y = "+ z);
+        Console.ReadKey();
+    }
+}
+}
